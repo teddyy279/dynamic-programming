deb293d32622c90221840ffd712945a5d24c05ba
teddy279 <phambaokhue2792004@gmail.com>
2024-04-22 21:02:17 +0700

    Update Problem1 Xâu con chung dài nhất.cpp

--- Problem1 Xâu con chung dài nhất.cpp
+++ Problem1 Xâu con chung dài nhất.cpp
@@ -4,27 +4,29 @@ using namespace std;
 
 int mod  = 1e9 + 7;
 
-int F[1001][1001];
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     string s1, s2;
     cin >> s1 >> s2;
+    int n = s1.length();
+    int m = s2.length();
     s1 = "0" + s1;
     s2 = "0" + s2;
+    int F[n + 1][m + 1];
+    memset(F, 0, sizeof(F));
     F[0][0] = 0;
-    for(int i = 1; i <= s1.length(); i++){
-        for(int j = 1; j <= s2.length(); j++){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
             if(s1[i] == s2[j]){
-                F[i][j] = F[i - 1][j - 1] + 1;
+                F[i][j] = max(F[i][j], F[i - 1][j - 1]  + 1);
             }
             else{
-                F[i][j] = max(F[i - 1][j], F[i][j - 1]);
+                F[i][j] = max(F[i - 1][j] , F[i][j - 1]);
             }
         }
     }
-    int n = s1.length();
-    int m = s2.length();
     cout << F[n][m];
 }
 
