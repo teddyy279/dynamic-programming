689704728aa017b5a2aaa2774b069c2c566b1bc4
teddy279 <phambaokhue2792004@gmail.com>
2024-04-23 20:45:50 +0700

    Create Problem8 Bài toán cái túi.cpp

---
+++ Problem8 Bài toán cái túi.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int mod  = 1e9 + 7;
+
+int F[101][1000001];
+
+int main(){
+    ios::sync_with_stdio(false);
+    int n, V;
+    cin >> n >> V;
+    int w[n + 1], c[n + 1];
+    for(int i = 1; i <= n; i++){
+        cin >> w[i];
+    }
+    for(int i = 1; i <= n; i++){
+        cin >> c[i];
+    }
+    F[0][0] = 0;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= V; j++){
+            if(j >= w[i]){
+            F[i][j] = max(F[i - 1][j - w[i]] + c[i], F[i - 1][j]);
+            }
+            else F[i][j] = F[i - 1][j];
+            F[i][j] %= mod;
+        }
+    }
+    cout << F[n][V];
+}
+
