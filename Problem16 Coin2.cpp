a6e93798645d2f3b43d2388b7a28386b2c9ef635
teddy279 <phambaokhue2792004@gmail.com>
2024-04-23 21:49:19 +0700

    Create Problem16 Coin2.cpp

---
+++ Problem16 Coin2.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int mod  = 1e9 + 7;
+
+long long F[1000001];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, x; cin >> n >> x;
+    int coin[n];
+    for(int i = 1; i <= n; i++){
+        cin >> coin[i];
+    }
+    F[0] = 1;
+    for(int i = 1; i <= x; i++){
+        for(int j = 1; j <= n; j++){
+            if(i >= coin[j]){
+            F[i] += F[i - coin[j]];
+            }
+            F[i] %= mod;
+        }
+    }
+    cout << F[x];
+}
+
